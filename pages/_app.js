d8c6dad43214d4a150d3599de5f167c306582e97
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-04-05 18:41:03 -0700

    Fetch data on server using getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,27 @@ import App from "next/app";
 import Layout from '../components/_App/Layout'
 
 class MyApp extends App {
+  // Static methods can be accessed outside of classes
+  // Get access to page component
+  static async getInitialProps({ Component, ctx }){
+    let pageProps = {};
+
+    if(Component.getInitialProps){
+      // Execute components getInitialProps function
+      // ctx - request/response information and current route we are on
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps }; // pageProps: pageProps
+  }
 
   // Component is returning the entire page
   // Pass component as child to layout
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
     return (
       <Layout>
-        <Component />
+        <Component {...pageProps} />
       </Layout>
       );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import axios from 'axios';
 
-function Home() {
-  // Interact with outside world, make request to an API
-  React.useEffect(() => {
-    getProducts();
-  }, []);
+function Home(props) {
+  console.log('Products ', props)
+  // Interact with outside world, make request to an API from client side
+  // Runs after componentDidMount
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
 
-  async function getProducts() {
-    const url = 'http://localhost:3000/api/products';
+  // async function getProducts() {
+  //   const url = 'http://localhost:3000/api/products';
 
-    const response = await axios.get(url);
+  //   const response = await axios.get(url);
 
-    console.log(response)
-  }
+  //   console.log(response)
+  // }
 
   return <>home</>;
+}
+
+// Special function from Next.js executed as a method on the Home component
+// Gets some initial data that will be merged to the props of this component
+Home.getInitialProps = async () => {
+  // Fetch data on server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+
+  // Return response data as an object
+  return { products: response.data };
 }
 
 export default Home;
